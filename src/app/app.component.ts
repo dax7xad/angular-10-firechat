b97864a5dc8f64165f24db91db24ea3b11d32939
Alvaro <dax7xad@gmail.com>
2018-09-15 20:24:18 -0600

    Finalizando el chat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { ChatService } from './services/chat.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,8 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
   title = 'firechat';
-  public chats: Observable<any[]>;
-  constructor(db: AngularFirestore) {
-    this.chats = db.collection('chats').valueChanges();
+  constructor(public _srvchats: ChatService) {
+
   }
 }
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -18,48 +18,60 @@ export class ChatService {
 
   constructor(private afs: AngularFirestore, public afAuth: AngularFireAuth) {
     this.afAuth.authState
-    .subscribe(user => {
-      console.log('Estado del usuario', user);
+      .subscribe(user => {
+        console.log('Estado del usuario', user);
 
-      if (!user) {
-        return;
-      }
+        if (!user) {
+          return;
+        }
 
-      this.usuario.nombre = user.displayName;
-      this.usuario.uid = user.uid;
+        this.usuario.nombre = user.displayName;
+        this.usuario.uid = user.uid;
 
-    });
+      });
   }
 
   login(proveedor: string) {
-    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+    switch (proveedor) {
+      case 'google':
+      this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+        break;
+      case 'github':
+      this.afAuth.auth.signInWithPopup(new auth.GithubAuthProvider());
+        break;
+      default:
+        break;
+    }
+
   }
   logout() {
+    this.usuario = {};
     this.afAuth.auth.signOut();
   }
 
   CargarMensajes() {
-     this.itemsCollection = this.afs.collection<IMensaje>('chats', ref => ref
-     .orderBy('fecha', 'desc')
-     .limit(5));
+    this.itemsCollection = this.afs.collection<IMensaje>('chats', ref => ref
+      .orderBy('fecha', 'desc')
+      .limit(5));
     // this.items = this.itemsCollection.valueChanges();
 
     return this.itemsCollection
-    .valueChanges()
-    .pipe(
-      map( (mensaje: IMensaje[]) => {
-      console.log(mensaje);
-      this.chats = mensaje.reverse();
-    })
-  );
+      .valueChanges()
+      .pipe(
+        map((mensaje: IMensaje[]) => {
+          console.log(mensaje);
+          this.chats = mensaje.reverse();
+        })
+      );
   }
 
-  AgregarMensaje (texto: string) {
+  AgregarMensaje(texto: string) {
 
     const mensaje: IMensaje = {
-      nombre: 'demo',
+      nombre: this.usuario.nombre,
       mensaje: texto,
-      fecha: new Date().getTime()
+      fecha: new Date().getTime(),
+      uid: this.usuario.uid
     };
     return this.itemsCollection.add(mensaje);
   }
